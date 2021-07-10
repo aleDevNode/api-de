5ba56bae6764d4504fc7a4d273dfeb7b6f0a6aaa
Alessandro <asovitorio@gmail.com>
2021-07-10 19:26:32 -0300

    :test: testando

--- testar.js
+++ testar.js
@@ -1,29 +1,31 @@
-const readline = require('readline');
-const opcoes = 0;
+// const readline = require('readline');
+// const opcoes = 0;
 
-const leitor = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const leitor = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-leitor.question("Escolha uma opção :\n1-levar uma surra\n2-levar outra surra\n3-bater um pouco\n-------------\n opt: ", (respota) => {
+// leitor.question("Escolha uma opção :\n1-levar uma surra\n2-levar outra surra\n3-bater um pouco\n-------------\n opt: ", (respota) => {
 
-    switch (parseInt(respota)) {
+//     switch (parseInt(respota)) {
 
-        case 1:
-            console.log("Apanhei muito meu Deus" );
-            break;
-        case 2:
-            console.log('Morri!');
-            break;
-        case 3:
-            console.log('Agora me vingo!');
-        break
-        default:
-            console.log('Briga encerrada'+typeof respota);
+//         case 1:
+//             console.log("Apanhei muito meu Deus" );
+//             break;
+//         case 2:
+//             console.log('Morri!');
+//             break;
+//         case 3:
+//             console.log('Agora me vingo!');
+//         break
+//         default:
+//             console.log('Briga encerrada'+typeof respota);
 
 
-    }
+//     }
 
-    leitor.close()
-})
+//     leitor.close()
+// })
+
+
