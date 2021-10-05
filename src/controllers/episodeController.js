5202e2406f53f5f96f026a775409e1e3a9dc032b
Alessandro <asovitorio@gmail.com>
2021-10-05 01:17:40 -0300

    adjust-test

--- src/controllers/episodeController.js
+++ src/controllers/episodeController.js
@@ -1,44 +1,66 @@
 const episodesDatabase = require('../database/services/episodesDatabase')
-const {Episode,File}= require('../models')
-const {Op} = require('sequelize');
+const {
+    Episode,
+    File
+} = require('../models')
+const {
+    Op
+} = require('sequelize');
 const episodesController = {
     index: async (req, res) => {
         try {
             // List all episodes whit pagination
-            const {episodes,total} = await episodesDatabase.findAllEpisodes(req.query);
-           return res.status(200).json({episodes,total});
+            const {
+                episodes,
+                total
+            } = await episodesDatabase.findAllEpisodes(req.query);
+            return res.status(200).json({
+                episodes,
+                total
+            });
         } catch (error) {
-            return res.status(400).json({msg:error});
+            return res.status(400).json({
+                msg: error
+            });
         }
     },
-   show: async (req, res)=>{
+    show: async (req, res) => {
         try {
             // Do Filter fo episode by id
-            if(Object.keys(req.params).length>0){
+            if (Object.keys(req.params).length > 0) {
                 const episode = await episodesDatabase.findByPkEpisode(req.params);
                 return res.status(200).json(episode);
-            } 
+            }
             // Do Filter fo episodes by parametros
-            if(Object.keys(req.query).length>0){
-                const {episodes,total} = await episodesDatabase.findSearch(req.query);
-                return res.status(200).json({episodes,total});
-            } 
+            if (Object.keys(req.query).length > 0) {
+                const {
+                    episodes,
+                    total
+                } = await episodesDatabase.findSearch(req.query);
+                return res.status(200).json({
+                    episodes,
+                    total
+                });
+            }
         } catch (error) {
             return res.status(400).json(error);
         }
     },
-    create: async(req, res) =>{
+    create: async (req, res) => {
         try {
-            
-            return res.status(200).json({teste:"teste"});
-            
+
+            return res.status(200).json({
+                teste: "teste"
+            });
+
         } catch (error) {
-            
-            return res.status(400).json({error});
+
+            return res.status(400).json({
+                error
+            });
         }
     }
 
 }
 
 module.exports = episodesController
-
