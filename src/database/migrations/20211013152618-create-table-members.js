546e3777422cc7a75e456a156b49c24fc4736ef3
Alessandro <asovitorio@gmail.com>
2021-10-13 12:48:59 -0300

    create migration membres

---
+++ src/database/migrations/20211013152618-create-table-members.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   
+      await queryInterface.createTable('members', { 
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4
+        },
+        name: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+          
+        },
+        function: {
+          type: Sequelize.STRING(255),
+          allowNull: false,
+        },
+        rf: {
+          type: Sequelize.STRING(255),
+          allowNull: false,
+        },
+       
+        fullName: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+          unique:true,
+        },
+        email: {
+          type: Sequelize.STRING(255),
+          allowNull: false,
+          unique:true,
+        },
+        cellphone: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+          unique:true,
+        },    
+        description: {
+          type: Sequelize.TEXT,
+                   
+        },    
+        status: {
+          type: Sequelize.BOOLEAN,
+          defaultValue:true
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      
+      });
+     
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('members');
+    
+  }
+};
