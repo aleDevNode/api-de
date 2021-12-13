63048c0876a9e483b622e2d10dfbf9335e0a6368
Alessandro <asovitorio@gmail.com>
2021-12-13 01:15:57 -0300

    adjust authAcess

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,11 +6,12 @@ const url = process.env.URI_FRONT_END_PASS
 const authController = {
     auth: async (req, res) => {
         try {
+            
             const token = await authDataBase.auth(req.body)
             
             return res.status(200).json(token)
         } catch (error) {
-            return res.status(401).json({error})
+            return res.status(401).json(error.message)
         }
     },
 
--- src/database/services/authDatabase.js
+++ src/database/services/authDatabase.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const userDatabase = require("./userDatabase");
-const { User,Member } = require("../../models");
+const { User,Member,File } = require("../../models");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const JwtKey = process.env.JWT_PASS;
@@ -8,6 +8,7 @@ const JwtKey = process.env.JWT_PASS;
 module.exports = {
   auth: async (body) => {
     const { login, password } = body;
+   
     const user = await User.findOne({
       where: {
         login,
@@ -24,16 +25,19 @@ module.exports = {
           }
       }
     });
- 
-   
     if (!user) throw "User or Password invalid! USER";
     if (!bcrypt.compareSync(password, user.password))
-      throw "User or Password invalid! PASS";
+    throw "User or Password invalid! PASS";
     const usuToken = {
       id: user.id,
       name: user.member.full_name,
+      login:user.login,
+      func:user.member.func,
       email: user.member.email,
+      avatar:user.member.file.path,
+      status:user.status
     };
+   
     const token = jwt.sign(usuToken, JwtKey, { expiresIn: "8h" });
     if (!token) throw "token invalid!";
     return { token };
