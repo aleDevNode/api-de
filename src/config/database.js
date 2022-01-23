631b1f5ba65827d8e95c0018f5fa49831f6b7b08
Alessandro <asovitorio@gmail.com>
2022-01-22 21:31:08 -0300

    adjust database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 module.exports = {
-  dialect: process.env.DB_DIALECT || 'mysql',
+  dialect: process.env.DB_DIALECT,
   // storage: './__tests__/database.sqlite',
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database:process.env.NODE_ENV?'api-de-teste':process.env.DB_DATABASE,
+  database:process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   timezone: "-03:00",
