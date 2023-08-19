7a7613c47e1ebd6378edb7ce2b385cfccdeaffd6
Alessandro <asovitorio@gmail.com>
2023-08-19 20:48:23 -0300

    subindo

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,24 @@
 require('dotenv').config()
 module.exports = {
   dialect: process.env.DB_DIALECT,
+  storage:'./db.sqlite'
   // storage: './__tests__/database.sqlite',
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database:process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  dialectOptions : { 
-    ssl : { 
-      rejectUnauthorized : false ,  // muito importante 
-    } 
-  },
-  timezone: "-03:00",
-  logging: false,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database:process.env.DB_DATABASE,
+  // host: process.env.DB_HOST,
+  // port: parseInt(process.env.DB_PORT),
+  // dialectOptions : { 
+  //   ssl : { 
+  //     rejectUnauthorized : false ,  // muito importante 
+  //   } 
+  // },
+  // timezone: "-03:00",
+  // logging: false,
+  // define: {
+  //   timestamps: true,
+  //   underscored: true,
+  //   underscoredAll: true,
     
-  }
+  // }
 }
